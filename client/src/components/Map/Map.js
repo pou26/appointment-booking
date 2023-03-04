55a0d5363255da29fd89047fb49a10b2f8d2c3d6
pou26 <poushaliaich1999@gmail.com>
2023-03-04 18:22:40 +0530

    final

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -19,7 +19,7 @@ export default function Map() {
                     <div className="img col-lg-4 col-md-12 mb-4 mb-lg-0">
                         <img
                             src="https://altiushospital.com/wp-content/uploads/2021/02/Aryabhata-International-Award.jpg"
-                            className="img-container w-100 shadow-1-strong rounded mb-4"
+                            className="img-containerr w-100 shadow-1-strong rounded mb-4"
                             alt="CT Scan & X-Ray"
                             
                         />
@@ -27,7 +27,7 @@ export default function Map() {
 
                         <img
                             src="https://altiushospital.com/wp-content/uploads/2021/02/Rashtriya-Rattan-Award.jpg"
-                            className="img-container w-100 shadow-1-strong rounded mb-4"
+                            className="img-containerr w-100 shadow-1-strong rounded mb-4"
                             alt="Cardiac Sciences"
                             
                         />
@@ -37,7 +37,7 @@ export default function Map() {
                     <div className="img col-lg-4 mb-4 mb-lg-0">
                         <img
                             src="https://altiushospital.com/wp-content/uploads/2021/02/Nada-Shilpi-Kempegowda-Award.jpg"
-                            className="img-container w-100 shadow-1-strong rounded mb-4"
+                            className="img-containerr w-100 shadow-1-strong rounded mb-4"
                             alt="Neuro Sciences"
                             
                         />
@@ -45,7 +45,7 @@ export default function Map() {
 
                         <img
                             src="https://altiushospital.com/wp-content/uploads/2021/02/J-P-Samajarathna-Award.jpg"
-                            className="img-container w-100 shadow-1-strong rounded mb-4"
+                            className="img-containerr w-100 shadow-1-strong rounded mb-4"
                             alt="Oncology"
                             
                         />
@@ -55,7 +55,7 @@ export default function Map() {
                     <div className="img col-lg-4 mb-4 mb-lg-0">
                         <img
                             src="https://altiushospital.com/wp-content/uploads/2021/02/National-Achievement-Award.jpg"
-                            className="img-container w-100 shadow-1-strong rounded mb-4"
+                            className="img-containerr w-100 shadow-1-strong rounded mb-4"
                             alt="Laparoscopy"
                             
                         />
@@ -63,7 +63,7 @@ export default function Map() {
 
                         <img
                             src="https://altiushospital.com/wp-content/uploads/2021/02/DSC_0071.jpg"
-                            className="img-container w-100 shadow-1-strong rounded mb-4"
+                            className="img-containerr w-100 shadow-1-strong rounded mb-4"
                             alt="Orthopaedics"
                             
                         />
@@ -72,7 +72,7 @@ export default function Map() {
                 </section>
 
                 <div className="gmap">
-                    <iframe width="600px" height="400px" id="gmap_canvas" src="https://maps.google.com/maps?q=altius hospital bangalore&t=&z=11&ie=UTF8&iwloc=&output=embed" scrolling="yes" ></iframe>
+                    <iframe width="600px" height="600px" id="gmap_canvas" src="https://maps.google.com/maps?q=altius hospital bangalore&t=&z=11&ie=UTF8&iwloc=&output=embed" scrolling="yes" ></iframe>
                 </div>
             </div>
             <div className="feedback">
